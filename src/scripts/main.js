be994015b9ef3855e0d5bcf4f8ac341fe50513fb
Dmitrii Starkov <starkoffbusiness@gmail.com>
2023-04-26 12:00:52 +0500

    add style for timer

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -14,12 +14,28 @@ const week = {
 grid.addEventListener("click", function (event) {
   if (event.target.classList.contains("grid__check")) {
     event.target.parentNode.classList.toggle("cross");
-    const square = window.getComputedStyle(event.target.parentNode, '::before');
+    const square = window.getComputedStyle(event.target.parentNode, "::before");
   }
 });
 
 (function () {
   const today = new Date();
   const todayPlan = document.querySelector(`#${week[today.getDay()]}`); // it doesnt work
-  todayPlan.classList.add('today');
+  todayPlan.classList.add("today");
 })();
+
+// code for timer
+
+const openButton = document.querySelector(".button__open");
+const hideButton = document.querySelector(".button__hide");
+const timer = document.querySelector(".timer");
+
+openButton.addEventListener("click", () => {
+  openButton.style.display = "none";
+  timer.style.display = "block";
+});
+
+hideButton.addEventListener("click", () => {
+  openButton.style.display = "flex";
+  timer.style.display = "none";
+});
